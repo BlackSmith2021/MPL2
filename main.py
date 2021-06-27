59b97830b5940a51a23903811fdb4bca5db55596
BlackSmith2021 <78790865+BlackSmith2021@users.noreply.github.com>
2021-06-27 17:49:57 +0400

    Добавлены функции считывания одного файла и нескольких

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-import numpy as np
 bc = mcolors.BASE_COLORS
 
-f = ['0001.lvm', '0002.lvm', '0004.lvm', '0005.lvm']
 d = []
 x = []
 y = []
@@ -13,45 +11,39 @@ x2 = []
 y2 = []
 x3 = []
 y3 = []
-sl = slice(None, 100)
 
-for i in f:
-    with open(i, 'r') as f:
-        read_file = f.readlines()
-        d.append(read_file)
+def read_one_file(f):
+    with open(f, 'r') as f_mf:
+        read_file = f_mf.readlines()
+        return read_file
 
-for i in range(len(d)):
-    for j in d[i]:
-        a = j.split('\t')
-        if i == 0:
-            x.append(a[0])
-            x = x[sl]
-            b = a[1]
-            y.append(b[0:-1])
-            y = y[sl]
-        if i == 1:
-            x1.append(a[0])
-            x1 = x1[sl]
-            b = a[1]
-            y1.append(b[0:-1])
-            y1 = y1[sl]
-        if i == 2:
-            x2.append(a[0])
-            x2 = x2[sl]
-            b = a[1]
-            y2.append(b[0:-1])
-            y2 = y2[sl]
-        if i == 3:
-            x3.append(a[0])
-            x3 = x3[sl]
-            b = a[1]
-            y3.append(b[0:-1])
-            y3 = y3[sl]
+def read_sever_files(f_s):
+    for i in f_s:
+        d.append(read_one_file(i))
+    for i in range(len(d)):
+        for j in d[i]:
+            a = j.split('\t')
+            if i == 0:
+                x.append(a[0])
+                y.append(a[1][0:-1])
+            if i == 1:
+                x1.append(a[0])
+                y1.append(a[1][0:-1])
+            if i == 2:
+                x2.append(a[0])
+                y2.append(a[1][0:-1])
+            if i == 3:
+                x3.append(a[0])
+                y3.append(a[1][0:-1])
 
-plt.plot(x, y, "g--")
-plt.plot(x1, y1, "r--")
-plt.plot(x2, y2, "b--")
-plt.plot(x3, y3, "w--")
+read_sever_files(['0001.lvm', '0002.lvm', '0004.lvm', '0005.lvm'])
+
+
+
+plt.plot(x[::100], y[::100], "g--")
+plt.plot(x1[::100], y1[::100], "r--")
+plt.plot(x2[::100], y2[::100], "b--")
+plt.plot(x3[::100], y3[::100], "o--")
 
 plt.show()
 
